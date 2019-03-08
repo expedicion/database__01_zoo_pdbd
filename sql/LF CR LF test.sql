cf466c7cd9f944ed34db6aef93abf416e945e8ad
Nostromo <mks.nostromo@gmail.com>
2019-03-08 17:02:25 +0100

    EOL set to CRLF.

--- sql/LF CR LF test.sql
+++ sql/LF CR LF test.sql
@@ -1,1 +1,15 @@
---- test file		-- line 1					-- line 2					-- line 3	to				-- line 4					-- line 5	delete ----		-- line 6
+--- test file		-- line 1
+					-- line 2
+					-- line 3
+	to				-- line 4
+					-- line 5
+	delete ----		-- line 6
+	
+--- test file 2
+
+	some new line	-- line 1
+					-- line 2
+	was added		-- line 3
+	
+	
+	
